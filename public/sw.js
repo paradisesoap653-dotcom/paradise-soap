9ec0bd1150f7f17e60955fb95d9e23f7d27e19f2
paradisesoap653-dotcom <paradisesoap653@gmail.com>
2026-08-06 15:04:15 +0200

    Refactor service worker installation and fetch events
    Removed cache management logic and simplified event listeners.

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,7 @@
-const CACHE_NAME = 'paradise-soap-v2';
-
-self.addEventListener('install', (event) => {
+self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
-  );
-});
-
 self.addEventListener('fetch', (event) => {
-  // Service worker basic fetch handler
+  // Service worker basic setup
 });
